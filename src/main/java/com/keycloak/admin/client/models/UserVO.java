925f2d7382affe5f2348e76b6fe88c9d04829c4e
cometbid-project <20684020+cometbid-project@users.noreply.github.com>
2023-09-09 08:46:56 -0400

    add sort to granted roles

--- src/main/java/com/keycloak/admin/client/models/UserVO.java
+++ src/main/java/com/keycloak/admin/client/models/UserVO.java
@@ -3,12 +3,16 @@
  */
 package com.keycloak.admin.client.models;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotBlank;
@@ -23,8 +27,11 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.WordUtils;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User.AuthorityComparator;
+import org.springframework.util.Assert;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -143,9 +150,46 @@ public class UserVO implements UserDetails {
 
 	@JsonIgnore
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return this.roles.stream().map(authority -> new SimpleGrantedAuthority(authority)).collect(Collectors.toSet());
+		return sortAuthorities(
+				this.roles.stream().map(authority -> 
+					new SimpleGrantedAuthority(authority)
+				)
+				.collect(Collectors.toSet())
+		);
 	}
 
+	private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
+		Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
+		// Ensure array iteration order is predictable (as per
+		// UserDetails.getAuthorities() contract and SEC-717)
+		SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<>(new AuthorityComparator());
+		for (GrantedAuthority grantedAuthority : authorities) {
+			Assert.notNull(grantedAuthority, "GrantedAuthority list cannot contain any null elements");
+			sortedAuthorities.add(grantedAuthority);
+		}
+		return sortedAuthorities;
+	}
+	
+	private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable {
+
+		private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
+		@Override
+		public int compare(GrantedAuthority g1, GrantedAuthority g2) {
+			// Neither should ever be null as each entry is checked before adding it to
+			// the set. If the authority is null, it is a custom authority and should
+			// precede others.
+			if (g2.getAuthority() == null) {
+				return -1;
+			}
+			if (g1.getAuthority() == null) {
+				return 1;
+			}
+			return g1.getAuthority().compareTo(g2.getAuthority());
+		}
+	}
+
+	
 	public String getDisplayName() {
 		return displayName(this.firstName, this.lastName);
 	}
